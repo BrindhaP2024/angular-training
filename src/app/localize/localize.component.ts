806831727864078c0e2d32a3a56752b79a4688cb
brindha.palani@aspiresys.com <brindha.palani@aspiresys.com>
2025-04-10 18:50:00 +0530

    internalization

---
+++ src/app/localize/localize.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-localize',
+  imports: [],
+  templateUrl: './localize.component.html',
+  styleUrl: './localize.component.css'
+})
+export class LocalizeComponent {
+
+}
