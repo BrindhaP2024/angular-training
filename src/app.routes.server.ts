890be65bc5f7838b4156ac7079d0220b269cfce0
brindha.palani@aspiresys.com <brindha.palani@aspiresys.com>
2025-04-10 18:44:07 +0530

    web workers,service-worker-pwa

---
+++ src/app.routes.server.ts
@@ -0,0 +1,20 @@
+// app.routes.server.ts
+import { RenderMode, ServerRoute } from '@angular/ssr';
+export const serverRoutes: ServerRoute[] = [
+  {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'event',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'pipes',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**',
+    renderMode: RenderMode.Server,
+  },
+];
