2e03f22503d3d324e944edb1e8225991e1afca65
brindha.palani@aspiresys.com <brindha.palani@aspiresys.com>
2025-04-10 18:36:08 +0530

    custom-elements

--- src/app/customelements/customelements.component.ts
+++ src/app/customelements/customelements.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Injector, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Injector, Input, Output, Inject, PLATFORM_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { createCustomElement } from '@angular/elements';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-customelements',
@@ -13,17 +14,24 @@ import { createCustomElement } from '@angular/elements';
 export class CustomelementsComponent {
   @Input() Input = 'Angular Calculator Component';
   @Output() output = new EventEmitter<number>();
-  @Input() name = '';
-  @Input() email = '';
-  @Input() role = '';
+  @Input() name = 'brindha';
+  @Input() email = 'brindha@gmail.com';
+  @Input() role = 'user';
 
   number1 = 0;
   number2 = 0;
   result = 0;
 
-  constructor(private injector: Injector) {
-    const userCardElement = createCustomElement(CustomelementsComponent, { injector });
-    customElements.define('user-card', userCardElement);
+  constructor(
+    private injector: Injector,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      if (!customElements.get('user-card')) {
+        const userCardElement = createCustomElement(CustomelementsComponent, { injector });
+        customElements.define('user-card', userCardElement);
+      }
+    }
   }
 
   add(): void {
